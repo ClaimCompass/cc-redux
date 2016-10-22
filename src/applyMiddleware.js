17bc71a3c4d8a203add7f76b4b23945e0f38b74b
Velizar Shulev <velizar.shulev@gmail.com>
2016-10-22 12:29:32 -0700

    Simplify applyMiddleware

--- src/applyMiddleware.js
+++ src/applyMiddleware.js
@@ -2,9 +2,5 @@ import { applyMiddleware as reduxApplyMiddleware, combine } from 'redux'
 import thunk from 'redux-thunk'
 
 export default function applyMiddleware (...additionalMiddlewares) {
-  return createStore => (reducer, initial, enhancer) => {
-    const middleware = reduxApplyMiddleware(thunk)
-    const enhancerFunc = enhancer ? combine(middleware, enhancer) : middleware
-    return createStore(reducer, initial, enhancerFunc)
-  }
+  return reduxApplyMiddleware(thunk, ...additionalMiddlewares)
 }

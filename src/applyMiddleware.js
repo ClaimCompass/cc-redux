8e8972f2795f3523318c5110981a6d00ae3befba
Velizar Shulev <velizar.shulev@gmail.com>
2016-10-21 17:00:31 -0700

    Fix combine order

--- src/applyMiddleware.js
+++ src/applyMiddleware.js
@@ -4,7 +4,7 @@ import thunk from 'redux-thunk'
 export default function applyMiddleware (...additionalMiddlewares) {
   return createStore => (reducer, initial, enhancer) => {
     const middleware = reduxApplyMiddleware(thunk)
-    const enhancerFunc = enhancer ? combine(enhancer, middleware) : middleware
+    const enhancerFunc = enhancer ? combine(middleware, enhancer) : middleware
     return createStore(reducer, initial, enhancerFunc)
   }
 }
